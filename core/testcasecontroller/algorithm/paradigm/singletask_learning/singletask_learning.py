e848c34f1b4df6997d04348bd8dc7be39a1f156d
g-k-s-03 <govindsingh97704@gmail.com>
2026-07-02 13:12:09 +0530

    fix(core/singletask): guard BASE_MODEL_URL assignment against None initial_model_url, Issue 574
    Signed-off-by: g-k-s-03 <govindsingh97704@gmail.com>

--- core/testcasecontroller/algorithm/paradigm/singletask_learning/singletask_learning.py
+++ core/testcasecontroller/algorithm/paradigm/singletask_learning/singletask_learning.py
@@ -117,7 +117,10 @@ class SingleTaskLearning(ParadigmBase):
 
     def _train(self, job, initial_model):
         train_output_dir = os.path.join(self.workspace, "output/train/")
-        os.environ["BASE_MODEL_URL"] = initial_model
+        if initial_model is not None:
+            os.environ["BASE_MODEL_URL"] = initial_model
+        else:
+            os.environ.pop("BASE_MODEL_URL", None)
 
         train_dataset = self.dataset.load_data(self.dataset.train_url, "train")
         job.train(train_dataset)
